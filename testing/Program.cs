b32cd24a4950a84e48076f7228fd0623c7b0e116
Tim Hanewich <tahanewich@live.com>
2025-12-27 14:04:55 -0500

    Updated test

--- testing/Program.cs
+++ testing/Program.cs
@@ -9,22 +9,32 @@ namespace testing
     {
         public static void Main(string[] args)
         {
-            ResponseRequest rr = new ResponseRequest();
-            rr.Model = "gpt-5-mini-testing";
-            rr.Messages.Add(new Message(Role.user, "Hello my friend."));
-
-            Tool CheckWeather = new Tool();
-            CheckWeather.Name = "CheckWeather";
-            CheckWeather.Description = "Check the weather for any zip code.";
-            CheckWeather.Parameters.Add(new ToolInputParameter("zip_code", "Zip code of the area you want to check the weather for"));
-            rr.Tools.Add(CheckWeather);
-
+            //Define the deployment
             Deployment d = new Deployment();
-            d.Endpoint = "https://ai-testaistudio030597089470.openai.azure.com/openai/responses?api-version=2025-04-01-preview";
-            d.ApiKey = "Ax5hHeaVUqSipUxMkrQoecnHRsqPGl289HuaEMshHTemFhQpZtSoJQQJ99BGACHYHv6XJ3w3AAAAACOGdUF4";
+            d.Endpoint = "https://ai-testaistudio020597089470.openai.azure.com/openai/responses?api-version=2025-04-01-preview";
+            d.ApiKey = "Ax5hHeaVUqSipUxMkr...";
 
+            //Create a response request (uses the Responses API)
+            ResponseRequest rr = new ResponseRequest();
+            rr.Model = "gpt-5-mini-testing"; //the name of your particular deployment in Foundry
+            rr.Inputs.Add(new Message(Role.user, "Parse out the first and last name and provide it to me in JSON like this format, as an example: {'first': 'Ron', 'last': 'Weasley'}.\n\n'Hi, my name is Harold Gargon."));
+            rr.RequestedFormat = ResponseFormat.JsonObject; //specify you want a JSON object output ('JSON mode')
+
+            //Call to API service
             Response r = d.CreateResponseAsync(rr).Result;
-            Console.WriteLine(JsonConvert.SerializeObject(r, Formatting.Indented));
+
+            //Print response info
+            Console.WriteLine("Response ID: " + r.Id);
+            Console.WriteLine("Input tokens consumed: " + r.InputTokensConsumed.ToString());
+            Console.WriteLine("Output tokens consumed: " + r.OutputTokensConsumed.ToString());
+            foreach (Exchange exchange in r.Outputs) //loop through all outputs (output could be a message, function call, etc.)
+            {
+                if (exchange is Message msg) //if this output is a Message
+                {
+                   Console.WriteLine("Response: " + msg.Text);
+                }
+            }
+
 
         }
     }
