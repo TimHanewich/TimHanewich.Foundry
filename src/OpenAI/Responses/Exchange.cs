d0cf955fd98c4a12614d507613ddc9e5af8d3c9d
Tim Hanewich <tahanewich@live.com>
2025-12-27 08:20:52 -0500

    Renamed dialog to exchange

---
+++ src/OpenAI/Responses/Exchange.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TimHanewich.AgentFramework.Responses
+{
+    public class Exchange //represents an exchange of data between the AI and the party using it (could be conversational message, a tool call, etc.)
+    {
+        public Exchange()
+        {
+            
+        }
+    }
+}
