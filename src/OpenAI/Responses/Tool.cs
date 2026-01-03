4d99db1af76ac5b68559dad2adc64adc3b2f2a4c
Tim Hanewich <tahanewich@live.com>
2026-01-03 10:25:50 -0500

    Extrapolate class 'Tool'

--- src/OpenAI/Responses/Tool.cs
+++ src/OpenAI/Responses/Tool.cs
@@ -7,65 +7,9 @@ namespace TimHanewich.Foundry.OpenAI.Responses
 {
     public class Tool
     {
-        public string Name {get; set;}
-        public string Description {get; set;}
-        public List<ToolInputParameter> Parameters {get; set;}
-
-        public Tool()
+        public virtual JObject ToJSON()
         {
-            Name = "";
-            Description = "";
-            Parameters = new List<ToolInputParameter>();
-        }
-
-        public Tool(string name, string description)
-        {
-            Name = name;
-            Description = description;
-            Parameters = new List<ToolInputParameter>();
-        }
-
-        public JObject ToJSON()
-        {
-            JObject ToReturn = new JObject();
-
-            //Type
-            ToReturn.Add("type", "function");
-
-            //Name
-            ToReturn.Add("name", Name);
-
-            //Description
-            ToReturn.Add("description", Description);
-
-            //parameters
-            JObject parameters = new JObject();
-            ToReturn.Add("parameters", parameters);
-            parameters.Add("type", "object");
-
-            //properties
-            JObject properties = new JObject();
-            parameters.Add("properties", properties);
-            foreach (ToolInputParameter tip in Parameters)
-            {
-                JObject InfoAboutThisTIP = new JObject();
-                properties.Add(tip.Name, InfoAboutThisTIP);
-                InfoAboutThisTIP.Add("type", tip.ParameterType);
-                InfoAboutThisTIP.Add("description", tip.Description);
-            }
-
-            //required
-            JArray required = new JArray();
-            parameters.Add("required", required);
-            foreach (ToolInputParameter tip in Parameters)
-            {
-                if (tip.Required)
-                {
-                    required.Add(tip.Name);  
-                }
-            }
-
-            return ToReturn;
+            return new JObject();
         }
     }
 }
