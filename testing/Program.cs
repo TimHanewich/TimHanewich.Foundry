70b5852f7561e233700cb88278da709aaf9d63e5
Tim Hanewich <tahanewich@live.com>
2026-01-03 07:40:13 -0500

    Updated test

--- testing/Program.cs
+++ testing/Program.cs
@@ -2,6 +2,7 @@
 using System.Runtime.Intrinsics.Arm;
 using TimHanewich.Foundry.OpenAI.Responses;
 using Newtonsoft.Json;
+using TimHanewich.Foundry;
 
 namespace testing
 {
@@ -9,33 +10,15 @@ namespace testing
     {
         public static void Main(string[] args)
         {
-            //Define the deployment
-            Deployment d = new Deployment();
-            d.Endpoint = "https://ai-testaistudio020597089470.openai.azure.com/openai/responses?api-version=2025-04-01-preview";
-            d.ApiKey = "Ax5hHeaVUqSipUxMkr...";
+            FoundryResource fr = new FoundryResource("https://myfoundry-resource.services.ai.azure.com");
+            fr.ApiKey = "6ElIJZ2jsMM...";
 
-            //Create a response request (uses the Responses API)
             ResponseRequest rr = new ResponseRequest();
-            rr.Model = "gpt-5-mini-testing"; //the name of your particular deployment in Foundry
-            rr.Inputs.Add(new Message(Role.user, "Parse out the first and last name and provide it to me in JSON like this format, as an example: {'first': 'Ron', 'last': 'Weasley'}.\n\n'Hi, my name is Harold Gargon."));
-            rr.RequestedFormat = ResponseFormat.JsonObject; //specify you want a JSON object output ('JSON mode')
+            rr.Model = "gpt-5-nano";
+            rr.Inputs.Add(new Message(Role.user, "Why is the sky blue?"));
 
-            //Call to API service
-            Response r = d.CreateResponseAsync(rr).Result;
-
-            //Print response info
-            Console.WriteLine("Response ID: " + r.Id);
-            Console.WriteLine("Input tokens consumed: " + r.InputTokensConsumed.ToString());
-            Console.WriteLine("Output tokens consumed: " + r.OutputTokensConsumed.ToString());
-            foreach (Exchange exchange in r.Outputs) //loop through all outputs (output could be a message, function call, etc.)
-            {
-                if (exchange is Message msg) //if this output is a Message
-                {
-                   Console.WriteLine("Response: " + msg.Text);
-                }
-            }
-
-
+            Response r = fr.CreateResponseAsync(rr).Result;
+            Console.WriteLine(JsonConvert.SerializeObject(r, Formatting.Indented));
         }
     }
 }
