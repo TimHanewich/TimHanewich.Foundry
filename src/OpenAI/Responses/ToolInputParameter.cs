636c118e1d13415d2136b53701caaa817af8db66
Tim Hanewich <tahanewich@live.com>
2026-01-03 12:53:43 -0500

    ToolCallOutput --> FunctionCallOutput

--- src/OpenAI/Responses/ToolInputParameter.cs
+++ src/OpenAI/Responses/ToolInputParameter.cs
@@ -2,14 +2,14 @@ using System;
 
 namespace TimHanewich.Foundry.OpenAI.Responses
 {
-    public class ToolInputParameter
+    public class FunctionInputParameter
     {
         public string Name {get; set;} //The name of the input parameter (JSON property name)
         public string ParameterType {get; set;} //i.e. string, number
         public string Description {get; set;} //Description of what this parameter is
         public bool Required {get; set;} //if this is a required parameter or not (means the LLM must fill it out)
 
-        public ToolInputParameter()
+        public FunctionInputParameter()
         {
             Name = "";
             ParameterType = "string";
@@ -17,7 +17,7 @@ namespace TimHanewich.Foundry.OpenAI.Responses
             Required = true;
         }
 
-        public ToolInputParameter(string name, string description, string parameter_type = "string")
+        public FunctionInputParameter(string name, string description, string parameter_type = "string")
         {
             Name = name;
             Description = description;
