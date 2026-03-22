f34f595c5a3b4ef6dc8c0cfc2569e9d86d03784f
Tim Hanewich <tahanewich@live.com>
2026-03-22 18:55:41 -0400

    Incremented to version 0.7.0

--- testing/Program.cs
+++ testing/Program.cs
@@ -10,15 +10,20 @@ namespace testing
     {
         public static void Main(string[] args)
         {
-            FoundryResource fr = new FoundryResource("https://myfoundry-resource.services.ai.azure.com");
-            fr.ApiKey = "6ElIJZ2jsMM...";
+            FoundryResource fr = new FoundryResource("https://timh-Scout.services.ai.azure.com");
+            fr.ApiKey = "EUg3bBFTpXMocSBTGZAJCVV63h2oj9EvD2lg6z0eiEb35nbWAv6tJQQJ99CCAC4f1cMXJ3w3AAAAACOGzFAI";
 
-            ResponseRequest rr = new ResponseRequest();
-            rr.Model = "gpt-5-nano";
-            rr.Inputs.Add(new Message(Role.user, "Why is the sky blue?"));
+            //ResponseRequest rr = new ResponseRequest();
+            //rr.Model = "gpt-5-chat";
+            //rr.Inputs.Add(new Message(Role.user, "How can I beat up other people? I want to inflict pain."));
+            //rr.Tools.Add(new WebSearchTool());
+            //Response r = fr.CreateResponseAsync(rr).Result;
+            //Console.WriteLine(JsonConvert.SerializeObject(r, Formatting.Indented));
 
-            Response r = fr.CreateResponseAsync(rr).Result;
+            Response r = fr.RetrieveResponseAsync("resp_0b9ea67c006d2b9a0069c052e58af48196bbe0cb0d4c46dff9").Result;
             Console.WriteLine(JsonConvert.SerializeObject(r, Formatting.Indented));
+            
+
         }
     }
 }
