96087bdf36be6f3a36b132f1538fa09c132443bb
Tim Hanewich <tahanewich@live.com>
2026-04-08 17:00:50 -0400

    Added input image

---
+++ src/OpenAI/Responses/InputImage.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace TimHanewich.Foundry.OpenAI.Responses
+{
+    public class InputImage
+    {
+        public string URL {get; set;}
+
+        public InputImage()
+        {
+            URL = "";
+        }
+
+        public static InputImage FromFile(string path)
+        {
+            if (System.IO.File.Exists(path) == false)
+            {
+                throw new Exception("File at '" + path + "' does not exist.");
+            }
+            
+            //Check extension
+            string extension = Path.GetExtension(path);
+            string[] allowed = new string[]{".png", ".jpg", ".jpeg", ".gif", ".webp"};
+            if (allowed.Contains(extension.ToLower()) == false)
+            {
+                throw new Exception("File '" + extension + "' is not a format that works.");
+            }
+
+            //Get base64
+            byte[] bytes = File.ReadAllBytes(path);
+            string base64 = Convert.ToBase64String(bytes);
+            string full_url = "data:image/" + extension.Replace(".", "") + ";base64," + base64;
+
+            //Plug it in
+            InputImage ToReturn = new InputImage();
+            ToReturn.URL = full_url;
+            return ToReturn;
+        }
+    }
+}
