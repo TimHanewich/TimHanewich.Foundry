1972bbd85cd895ad5e87a0afc42cc158c33e1f90
Tim Hanewich <tahanewich@live.com>
2026-04-09 15:17:14 -0400

    Additional properties

--- src/OpenAI/Responses/JsonSchema.cs
+++ src/OpenAI/Responses/JsonSchema.cs
@@ -7,13 +7,13 @@ namespace TimHanewich.Foundry.OpenAI.Responses
     {
         public string Name {get; set;}
         public bool Strict {get; set;}
-        public JObject? Schema {get; set;}
+        public JObject Schema {get; set;}
 
         public JsonSchema()
         {
             Name = string.Empty;
             Strict = true;
-            Schema = null;
+            Schema = new JObject();
         }
 
         public JObject ToJSON()
@@ -23,8 +23,22 @@ namespace TimHanewich.Foundry.OpenAI.Responses
             //type
             ToReturn.Add("type", "json_schema");
 
+            //name
+            ToReturn.Add("name", Name);
+
             //strict
             ToReturn.Add("strict", true);
+
+            //Ensure schema has "additionalProperties" and it is set to false (prob doesnt have it)
+            JProperty? prop_additionalProperties = Schema.Property("additionalProperties");
+            if (prop_additionalProperties == null)
+            {
+                Schema.Add("additionalProperties", false);
+            }
+            else
+            {
+                Schema["additionalProperties"] = false;
+            }
 
             //schema
             ToReturn.Add("schema", Schema);
