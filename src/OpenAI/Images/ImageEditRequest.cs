92eb6ad956960aceec2f4576f4e1afa26c7d23a3
Tim Hanewich <tahanewich@live.com>
2026-05-09 13:50:53 -0400

    Inherit

--- src/OpenAI/Images/ImageEditRequest.cs
+++ src/OpenAI/Images/ImageEditRequest.cs
@@ -2,58 +2,13 @@ using System;
 
 namespace TimHanewich.Foundry.OpenAI.Images
 {
-    public class ImageEditRequest
+    public class ImageEditRequest : ImageRequest
     {
-        public string Model {get; set;}
-        public string Prompt {get; set;}
-        public int Width {get; set;}
-        public int Height {get; set;}
-        public int Count {get; set;}
-        public ImageQuality Quality {get; set;}
         public List<AttachedImage> AttachedImages {get; set;}
 
         public ImageEditRequest()
         {
-            Model = string.Empty;
-            Prompt = string.Empty;
             AttachedImages = new List<AttachedImage>();
-            Count = 1;
-        }
-
-        public void SetCommonSize(ImageSize size)
-        {
-            if (size == ImageSize.Size_1024x1024)
-            {
-                Width = 1024; Height = 1024;
-            }
-            else if (size == ImageSize.Size_1536x1024)
-            {
-                Width = 1536; Height = 1024;
-            }
-            else if (size == ImageSize.Size_1024x1536)
-            {
-                Width = 1024; Height = 1536;
-            }
-            else if (size == ImageSize.Size_2048x2048)
-            {
-                Width = 2048; Height = 2048;
-            }
-            else if (size == ImageSize.Size_2048x1152)
-            {
-                Width = 2048; Height = 1152;
-            }
-            else if (size == ImageSize.Size_3840x2160)
-            {
-                Width = 3840; Height = 2160;
-            }
-            else if (size == ImageSize.Size_2160x3840)
-            {
-                Width = 2160; Height = 3840;
-            }
-            else
-            {
-                Width = 1024; Height = 1024;
-            }
         }
 
         //Prepare
--- src/OpenAI/Images/ImageGenerationRequest.cs
+++ src/OpenAI/Images/ImageGenerationRequest.cs
@@ -6,57 +6,10 @@ using Newtonsoft.Json;
 
 namespace TimHanewich.Foundry.OpenAI.Images
 {
-    public class ImageGenerationRequest
+    public class ImageGenerationRequest : ImageRequest
     {
-        public string Model {get; set;}
-        public int Width {get; set;}
-        public int Height {get; set;}
-        public string Prompt {get; set;}
-        public int Count {get; set;}
-        public ImageQuality Quality {get; set;}
-
         public ImageGenerationRequest()
         {
-            Model = string.Empty;
-            Prompt = string.Empty;
-            Count = 1; //default
-            Quality = ImageQuality.Auto;
-        }
-
-        public void SetCommonSize(ImageSize size)
-        {
-            if (size == ImageSize.Size_1024x1024)
-            {
-                Width = 1024; Height = 1024;
-            }
-            else if (size == ImageSize.Size_1536x1024)
-            {
-                Width = 1536; Height = 1024;
-            }
-            else if (size == ImageSize.Size_1024x1536)
-            {
-                Width = 1024; Height = 1536;
-            }
-            else if (size == ImageSize.Size_2048x2048)
-            {
-                Width = 2048; Height = 2048;
-            }
-            else if (size == ImageSize.Size_2048x1152)
-            {
-                Width = 2048; Height = 1152;
-            }
-            else if (size == ImageSize.Size_3840x2160)
-            {
-                Width = 3840; Height = 2160;
-            }
-            else if (size == ImageSize.Size_2160x3840)
-            {
-                Width = 2160; Height = 3840;
-            }
-            else
-            {
-                Width = 1024; Height = 1024;
-            }
         }
 
         public JObject ToJSON()
