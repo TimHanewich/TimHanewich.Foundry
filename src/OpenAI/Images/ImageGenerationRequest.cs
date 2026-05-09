77b5bdc7a5c0f226eb6cd7053ac424fd418e38c0
Tim Hanewich <tahanewich@live.com>
2026-05-09 13:46:43 -0400

    Fill in size

--- src/OpenAI/Images/ImageGenerationRequest.cs
+++ src/OpenAI/Images/ImageGenerationRequest.cs
@@ -25,7 +25,38 @@ namespace TimHanewich.Foundry.OpenAI.Images
 
         public void SetCommonSize(ImageSize size)
         {
-            //Set width + height accordingly
+            if (size == ImageSize.Size_1024x1024)
+            {
+                Width = 1024; Height = 1024;
+            }
+            else if (size == ImageSize.Size_1536x1024)
+            {
+                Width = 1536; Height = 1024;
+            }
+            else if (size == ImageSize.Size_1024x1536)
+            {
+                Width = 1024; Height = 1536;
+            }
+            else if (size == ImageSize.Size_2048x2048)
+            {
+                Width = 2048; Height = 2048;
+            }
+            else if (size == ImageSize.Size_2048x1152)
+            {
+                Width = 2048; Height = 1152;
+            }
+            else if (size == ImageSize.Size_3840x2160)
+            {
+                Width = 3840; Height = 2160;
+            }
+            else if (size == ImageSize.Size_2160x3840)
+            {
+                Width = 2160; Height = 3840;
+            }
+            else
+            {
+                Width = 1024; Height = 1024;
+            }
         }
 
         public JObject ToJSON()
